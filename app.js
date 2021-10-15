d8885ead23b0562ecfadaff9edf15cdb46e3b7e1
Nathan Yee <nyee@csu.fullerton.edu>
2021-10-15 14:34:55 -0700

    Converted code to use MongoDB

--- app.js
+++ app.js
@@ -4,26 +4,42 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+// Express setup
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// array containing blog post object with title and content
-const blogPosts = [];
+
+// Mongoose DB setup
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
 
 
 app.get("/", (req, res) =>{
 
+  Blog.find({}, (err, posts) =>{
+    if(err){
+      console.log(err);
+    } else{
+      res.render('home', {viewHomeContent: homeStartingContent, viewBlogPost: posts});
 
-  res.render('home', {viewHomeContent: homeStartingContent, viewBlogPost: blogPosts});
+    }
+  });
+
+  // res.render('home', {viewHomeContent: homeStartingContent, viewBlogPost: blogPosts});
 
 });
 
@@ -44,38 +60,49 @@ app.post("/compose", (req, res) => {
   let blogTitle = req.body.blogTitle;
   let blogContent = req.body.blogContent;
 
-  const post = {
+  const newPost = new Blog({
     title: blogTitle,
     content: blogContent
-  };
+  });
 
-  blogPosts.push(post);
+  newPost.save((err) =>{
+    if(!err){
+      res.redirect("/");
+    }
+  });
 
 
-  res.redirect("/")
 
 });
 
-app.get("/posts/:postName", (req, res) => {
+app.get("/posts/:postId", (req, res) => {
 
-  let postName = req.params.postName;
+  // let postName = req.params.postName;
 
   // set postName to kebab case
-  postName = _.kebabCase(postName);
+  // postName = _.kebabCase(postName);
   
+  const requestedPostId = req.params.postId;
 
-  // if post exists, set marker to true
-  blogPosts.forEach((post) => {
-
-    postTitle = _.kebabCase(post.title);
-
-    if(postName === postTitle) {
-      
-      //const postURL = "posts/" + postName;
-      
-      res.render('post', { webPostTitle: post.title, webPostContent: post.content });      
-    }
+  Blog.findOne({_id: requestedPostId}, (err, post) =>{
+    res.render("post", {
+      webPostTitle: post.title,
+      webPostContent: post.content
+    });
   });
+
+  // // if post exists, set marker to true
+  // blogPosts.forEach((post) => {
+
+  //   postTitle = _.kebabCase(post.title);
+
+  //   if(postName === postTitle) {
+      
+  //     //const postURL = "posts/" + postName;
+      
+  //     res.render('post', { webPostTitle: post.title, webPostContent: post.content });      
+  //   }
+  // });
 
 });
 
